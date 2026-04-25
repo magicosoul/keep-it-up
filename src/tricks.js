ba1baff4ef9f52b5a4ca0ea87c86a2060c4c56e2
magicosoul <freakyflow14@gmail.com>
2026-04-26 01:10:55 +0900

    Add trick definitions placeholder

---
+++ src/tricks.js
@@ -0,0 +1,26 @@
+export const BODY_PARTS = {
+  LEFT_FOOT: 'leftFoot',
+  RIGHT_FOOT: 'rightFoot',
+  CHEST: 'chest',
+  HEAD: 'head',
+};
+
+export const PHASE_ONE_MOVES = {
+  leftFoot: {
+    label: 'Left Foot',
+    score: 1,
+  },
+  rightFoot: {
+    label: 'Right Foot',
+    score: 1,
+  },
+};
+
+export const FUTURE_TRICKS = [
+  'Around the World',
+  'Crossover',
+  'Heel Flick',
+  'Backheel',
+  'Chest Pop',
+  'Neck Stall',
+];
